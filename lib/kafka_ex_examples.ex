da4c5761b23e9b8f9aa72e8a94ba31a97b2d0c60
Dan Swain <dan.t.swain@gmail.com>
2017-07-26 16:07:04 -0400

    Set some of the consumer group options

--- lib/kafka_ex_examples.ex
+++ lib/kafka_ex_examples.ex
@@ -10,10 +10,17 @@ defmodule KafkaExExamples do
   def start(_type, _args) do
     import Supervisor.Spec
 
+    consumer_group_opts = [
+      commit_interval: 1_000,
+      heartbeat_interval: 1_000,
+      # must be >= group.min.session.timeout.ms from broker config
+      session_timeout: 6_000
+    ]
+
     children = [
       supervisor(
         KafkaEx.ConsumerGroup.Supervisor,
-        [Consumer, "example_group", ["example_topic"]]
+        [Consumer, "example_group", ["example_topic"], consumer_group_opts]
       )
     ]
     supervisor_opts = [strategy: :one_for_one]

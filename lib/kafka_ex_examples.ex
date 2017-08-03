0fe5dee28bfc2e8d3d2cb270f12697dbc1926d7f
Dan Swain <dan.t.swain@gmail.com>
2017-08-03 19:25:13 -0400

    Update to latest kafkaex commit

--- lib/kafka_ex_examples.ex
+++ lib/kafka_ex_examples.ex
@@ -5,7 +5,11 @@ defmodule KafkaExExamples do
 
   use Application
 
+  alias KafkaEx.ConsumerGroup
+  alias KafkaEx.ConsumerGroup.PartitionAssignment
   alias KafkaExExamples.Consumer
+
+  require Logger
 
   def start(_type, _args) do
     import Supervisor.Spec
@@ -13,18 +17,29 @@ defmodule KafkaExExamples do
     consumer_group_opts = [
       commit_interval: 1_000,
       heartbeat_interval: 1_000,
+      name: ExampleConsumerGroup,
+      gen_server_opts: [name: ExampleConsumerGroup.Manager],
+      partition_assignment_callback: &assign_partitions/2,
       # must be >= group.min.session.timeout.ms from broker config
       session_timeout: 6_000
     ]
 
     children = [
       supervisor(
-        KafkaEx.ConsumerGroup.Supervisor,
+        KafkaEx.ConsumerGroup,
         [Consumer, "example_group", ["example_topic"], consumer_group_opts]
       )
     ]
     supervisor_opts = [strategy: :one_for_one]
 
     Supervisor.start_link(children, supervisor_opts)
+  end
+
+  def assign_partitions(members, partitions) do
+    result = PartitionAssignment.round_robin(members, partitions)
+    Logger.debug(fn ->
+      "ASSIGN: #{inspect members} | #{inspect partitions} | #{inspect result}"
+    end)
+    result
   end
 end
--- lib/kafka_ex_examples/consumer.ex
+++ lib/kafka_ex_examples/consumer.ex
@@ -5,16 +5,10 @@ defmodule KafkaExExamples.Consumer do
 
   require Logger
 
-  def handle_message(message = %Message{}, consumer_state) do
-    Logger.debug(fn -> "GOT: #{inspect message}" end)
+  def handle_message_set(messages, consumer_state) do
+    for  message = %Message{} <- messages do
+      Logger.debug(fn -> "GOT: #{inspect message}" end)
+    end
     {:async_commit, consumer_state}
-  end
-
-  def assign_partitions(members, partitions) do
-    result = super(members, partitions)
-    Logger.debug(fn ->
-      "ASSIGN: #{inspect members} | #{inspect partitions} | #{inspect result}"
-    end)
-    result
   end
 end

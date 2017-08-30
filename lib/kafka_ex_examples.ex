bebc078f6b1d07e4233117f6433c99725d1c176e
Dan Swain <dan.t.swain@gmail.com>
2017-08-30 17:07:26 -0400

    Move to a single file

--- lib/kafka_ex_examples.ex
+++ lib/kafka_ex_examples.ex
@@ -3,6 +3,21 @@ defmodule KafkaExExamples do
   Documentation for KafkaExExamples.
   """
 
+  defmodule KafkaExExamples.Consumer do
+    use KafkaEx.GenConsumer
+
+    alias KafkaEx.Protocol.Fetch.Message
+
+    require Logger
+
+    def handle_message_set(messages, consumer_state) do
+      for  message = %Message{} <- messages do
+        Logger.debug(fn -> "GOT: #{inspect message}" end)
+      end
+      {:async_commit, consumer_state}
+    end
+  end
+
   use Application
 
   alias KafkaEx.ConsumerGroup.PartitionAssignment
